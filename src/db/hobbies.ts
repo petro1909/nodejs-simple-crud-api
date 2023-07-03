49767a883253c144c9f13307467dc62287bddff5
petro1909 <petya-97_97@mail.ru>
2023-07-03 15:20:56 +0300

    feat: add array with user hobbies

---
+++ src/db/hobbies.ts
@@ -0,0 +1,1 @@
+export const interests = ['3D printing', 'Amateur radio', 'Scrapbook', 'Amateur radio', 'Acting', 'Baton twirling', 'Board games', 'Book restoration', 'Cabaret', 'Calligraphy', 'Candle making', 'Computer programming', 'Coffee roasting'];
