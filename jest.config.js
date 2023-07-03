335984ea6c2e02aeb692201e6ca1aa6d49c5f89f
petro1909 <petya-97_97@mail.ru>
2023-07-03 15:06:53 +0300

    refactor: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/?(*.)+(test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   restoreMocks: true,
   resetMocks: true,
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleDirectories: ['node_modules', '<rootDir>/test'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
