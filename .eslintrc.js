a9af48e645a4236152a9ff6b58b8e94a0585b49a
petro1909 <petya-97_97@mail.ru>
2023-07-03 15:07:19 +0300

    refactor: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,13 @@ module.exports = {
       "error",
       {
         "endOfLine": "auto",
+        "singleQoute": "auto",
       }
     ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   },
 };
