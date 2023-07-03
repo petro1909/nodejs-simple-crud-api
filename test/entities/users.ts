12403bbd495eed21d205c4e67ba8c713f157e2b2
petro1909 <petya-97_97@mail.ru>
2023-07-03 15:22:24 +0300

    feat: add user list for testing

---
+++ test/entities/users.ts
@@ -0,0 +1,22 @@
+import { User } from 'src/types/user';
+
+export const users: Array<User> = [
+  {
+    id: '2ed85ed3-7ac5-49d3-a87f-af45a622b93e',
+    username: 'Aleksandar',
+    age: 38,
+    hobbies: ['Amateur radio'],
+  },
+  {
+    id: '7522228b-2e45-46d0-8656-50b606a76170',
+    username: 'Cayden',
+    age: 6,
+    hobbies: ['Computer programming', 'Computer programming', 'Candle making'],
+  },
+  {
+    id: 'ba276c59-78e8-4fb0-b499-d54db43f6575',
+    username: 'Bogdan',
+    age: 5,
+    hobbies: ['Baton twirling', 'Baton twirling'],
+  },
+];
